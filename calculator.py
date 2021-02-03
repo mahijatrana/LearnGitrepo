fec72f86f6e3fb4f9cbb9a6897dc1faf139fb6d6
mahirana <mahijatrana29@gmail.com>
2021-02-03 13:30:47 +0530

    initial commit

---
+++ calculator.py
@@ -0,0 +1,19 @@
+creator = "Mahi Rana"
+PI = "3.14768"
+
+def subtract(a,b):
+    return a-b
+
+def add(a,b):
+    return a+b
+
+def area(radius):
+    return PI*radius*radius
+
+print(__name__)
+
+if __name__ == "__main__":
+    print("This will only run when calculator is being executed as a script")
+    print(subtract(3, 5))
+
+_year = 2020
